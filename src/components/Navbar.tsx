62860350e714443a573902aadee538604dfa225e
davidedotzero <larron959@gmail.com>
2024-07-18 22:48:48 +0700

    -

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     return (
         <div className='h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative'>
             {/* MOBILE SCREEN */}
-            <div className="h-full flex item-center justify-between md:hidden">
+            <div className="h-full py-4 flex items-center justify-between md:hidden">
                 <Link href="/">
                     <div className="text-2xl tracking-wide">LAMA</div></Link>
                 <Menu />
